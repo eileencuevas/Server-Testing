c156288caee06869da608ac48138853e0587b4c1
steelworx <eileencuevas62@gmail.com>
2019-01-31 17:38:37 -0500

    added db access; GET endpoint for '/' for testing purposes

--- api/server.js
+++ api/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const knex = require('knex');
+const knexConfig = require('../knexfile');
 
+const db = knex(knexConfig.development);
 const server = express();
+
+server.use(express.json());
+
+server.get('/', async (req, res) => {
+    res.status(200).json({ message: 'Api working' });
+});
 
 module.exports = server;
