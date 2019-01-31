5437998867776d5ca17d08a74f277013d978ce8f
steelworx <eileencuevas62@gmail.com>
2019-01-31 17:49:05 -0500

    added GET /army/ endpoint

--- api/server.js
+++ api/server.js
@@ -11,4 +11,15 @@ server.get('/', async (req, res) => {
     res.status(200).json({ message: 'Api working' });
 });
 
+server.get('/army/', async (req, res) => {
+    try {
+
+        const members = await db('army');
+        
+        res.status(200).json(members);
+    } catch (err) {
+        res.status(500).json({ error: 'Seems like the army moved on.' });
+    }
+});
+
 module.exports = server;
