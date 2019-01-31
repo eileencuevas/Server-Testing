830cf3d9aa78ca352a8c286bd30e6cce963907ae
steelworx <eileencuevas62@gmail.com>
2019-01-31 17:38:53 -0500

    added tests for GET / endpoint to see if api works

--- api/server.spec.js
+++ api/server.spec.js
@@ -4,8 +4,18 @@ const server = require('./server.js');
 
 describe('server.js tests', () => {
     describe('GET / endpoint', () => {
-        // should show { message: 'Api working' }
-        //should return status of 200
+        it(`should return an object with 'Api working' when successful`, async () => {
+            let response = await request(server).get('/');
+            
+            expect(response.type).toMatch(/json/i);
+            expect(response.body).toEqual({ message: 'Api working' });
+        })
+
+        it(`should return a status code of 200 when successful`, async () => {
+            let response = await request(server).get('/');
+
+            expect(response.status).toBe(200);
+        })
     });
     describe('GET /army/ endpoint', () => {
         // should give response of 200 when sucessful
